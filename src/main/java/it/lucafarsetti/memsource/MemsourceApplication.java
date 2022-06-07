6dfcab9d135424ea8211b37a892269e3aa690dd0
Luca Farsetti <luca.farsetti@lascaux.it>
2022-06-07 17:47:25 +0200

    initial commit

---
+++ src/main/java/it/lucafarsetti/memsource/MemsourceApplication.java
@@ -0,0 +1,13 @@
+package it.lucafarsetti.memsource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemsourceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemsourceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/lucafarsetti/memsource/MemsourceApplicationTests.java
@@ -0,0 +1,13 @@
+package it.lucafarsetti.memsource;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemsourceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
